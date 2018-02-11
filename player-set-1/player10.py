3e1fa520b0b502acc410244b8c2a96cbabc2fafa
sivaraam <18676830+sivarama-subramanian@users.noreply.github.com>
2018-02-11 11:22:46 +0530

    added 10

---
+++ player-set-1/player10.py
@@ -0,0 +1,29 @@
+word1=input()
+word2=input()
+dict={}
+alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for char in alphabet:
+    dict[char]=0
+
+for ch in word1:
+    dict[ch]+=1
+for ch in word2:
+    dict[ch]-=1
+
+flag=False
+sum=0
+
+for char in dict:
+    count=dict[char]
+    sum+=count
+    if count == 1:
+        flag=True
+    if flag and count>1:
+        flag=False
+        break
+
+if flag and sum==0:
+    print("Yes")
+else:
+    print("No")
+
