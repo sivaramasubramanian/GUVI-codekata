cb545cad65334adae5f3d2d1c56caab6052b4761
sivaraam <18676830+sivarama-subramanian@users.noreply.github.com>
2018-02-11 18:00:03 +0530

    minor edit

--- player-set-2/player14.py
+++ player-set-2/player14.py
@@ -1,4 +1,7 @@
-word=input()
-vowels=('a','e','i','o','u')
-no_vowels="".join([ch for ch in reversed(word) if ch.lower() not in vowels])
-print(no_vowels)
+try:
+    word=input()
+    vowels=('a','e','i','o','u')
+    no_vowels="".join([ch for ch in reversed(word) if ch.lower() not in vowels])
+    print(no_vowels)
+except:
+    print("invalid")
