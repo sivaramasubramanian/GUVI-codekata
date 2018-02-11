18110021d869a438794bc01d7547c7953616d68f
sivaraam <18676830+sivarama-subramanian@users.noreply.github.com>
2018-02-11 17:57:52 +0530

    added invalid clause

--- player-set-2/player13.py
+++ player-set-2/player13.py
@@ -1,7 +1,10 @@
-num=int(input())
-sum_of_squares=0
-while num >0:
-    last_digit=(num%10)
-    sum_of_squares+=(last_digit**2)
-    num//=10
-print(sum_of_squares)
+try:
+    num=int(input())
+    sum_of_squares=0
+    while num >0:
+        last_digit=(num%10)
+        sum_of_squares+=(last_digit**2)
+        num//=10
+    print(sum_of_squares)
+except:
+    print("invalid")
